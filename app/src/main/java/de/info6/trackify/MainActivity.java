1bcc72a9dfaab5492fce2f543576dddba684f617
Sören <s.jannsem@raumobil.com>
2023-06-12 12:50:15 +0200

    More Gradle/ SDK Stuff

--- app/src/main/java/de/info6/trackify/MainActivity.java
+++ app/src/main/java/de/info6/trackify/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonNeueFahrt, buttonBisherigeFahrten, buttonExportieren;
