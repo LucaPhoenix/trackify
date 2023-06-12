8558d159b41d7fbef6b12e228247d5a7ac693d54
Sören <s.jannsem@raumobil.com>
2023-06-12 15:09:17 +0200

    profil button führt zu profil bearbeiten

--- app/src/main/java/de/info6/trackify/Profil.java
+++ app/src/main/java/de/info6/trackify/Profil.java
@@ -3,7 +3,9 @@ package de.info6.trackify;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -30,5 +32,13 @@ public class Profil extends AppCompatActivity {
         editText_hauptverkehrsmittel = findViewById(R.id.editText_hauptverkehrsmittel);
 
         button_speichernUserProfil = findViewById(R.id.button_speichernProfil);
+
+        button_speichernUserProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Profil.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
