586b2ea25b3cd74edccc562ca833e84a2cf4bb5e
Sören <s.jannsem@raumobil.com>
2023-07-17 17:34:51 +0200

    Scroll View eingebaut, weill auf kleinen geräten nicht alles zu sehen

--- app/src/main/java/de/info6/trackify/NeueFahrtStartzeit.java
+++ app/src/main/java/de/info6/trackify/NeueFahrtStartzeit.java
@@ -15,7 +15,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.type.LatLng;
 import com.nabinbhandari.android.permissions.PermissionHandler;
 import com.nabinbhandari.android.permissions.Permissions;
 
