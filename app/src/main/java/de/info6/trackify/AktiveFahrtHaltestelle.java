084cf5be19db908623f968db382819bdf1ec96ed
Sören <s.jannsem@raumobil.com>
2023-07-17 11:02:01 +0200

    , Profil Activity Scroll View eingebaut, weill auf kleinen geräten nicht alles zu sehen

--- app/src/main/java/de/info6/trackify/AktiveFahrtHaltestelle.java
+++ app/src/main/java/de/info6/trackify/AktiveFahrtHaltestelle.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
