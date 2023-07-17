273bfc6bc3609002a00ae3e0e5d98fd7c5039bbc
Sören <s.jannsem@raumobil.com>
2023-07-17 18:52:49 +0200

    FahrtBeenden soweit fertig

--- app/src/main/java/de/info6/trackify/FahrtBeenden.java
+++ app/src/main/java/de/info6/trackify/FahrtBeenden.java
@@ -11,6 +11,7 @@ import android.graphics.Bitmap;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
@@ -20,15 +21,17 @@ import android.widget.Toast;
 import com.nabinbhandari.android.permissions.PermissionHandler;
 import com.nabinbhandari.android.permissions.Permissions;
 
+import java.util.HashMap;
 import java.util.UUID;
 
-public class FahrtBeenden extends AppCompatActivity {
+public class FahrtBeenden extends AppCompatActivity implements OpenTimePicker{
 
-    TextView editText_endzeitFahrt, editText_ankunftZiel;
+    TextView editText_endzeitFahrt, editText_ankunftZiel, editText_haltestelleZiel;
 
-    Button button_speichernFahrtBeenden;
+    Button button_speichernFahrtBeenden, button_endzeitFahrt, button_ankunftZiel;
 
     Switch switch_fahrtExportieren;
+    HashMap<String, Object> collectedData = new HashMap<>();
 
     double latDouble, lonDouble;
 
@@ -46,10 +49,23 @@ public class FahrtBeenden extends AppCompatActivity {
         //Edit Text und Button initialisieren
         editText_endzeitFahrt = findViewById(R.id.editText_endzeitFahrt);
         editText_ankunftZiel = findViewById(R.id.editText_ankunftZiel);
+        editText_haltestelleZiel = findViewById(R.id.editText_HaltestelleZiel);
 
         button_speichernFahrtBeenden = findViewById(R.id.button_speichernFahrtBeenden);
+        button_endzeitFahrt = findViewById(R.id.button_endzeitFahrt);
+        button_ankunftZiel = findViewById(R.id.button_ankunftZiel);
 
         switch_fahrtExportieren = findViewById(R.id.switch_fahrtExportieren);
+
+        //Intent abrufen
+        Intent intentStart = getIntent();
+
+        if(intentStart != null){
+            collectedData = (HashMap<String, Object>) getIntent().getSerializableExtra("CollectedData");
+            if(collectedData != null) {
+                Log.d("----TAG", collectedData.toString());
+            }
+        }
 
 
         Permissions.check(this, Manifest.permission.ACCESS_FINE_LOCATION, null, new PermissionHandler() {
@@ -85,17 +101,41 @@ public class FahrtBeenden extends AppCompatActivity {
         button_speichernFahrtBeenden.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (editText_endzeitFahrt.getText().toString() == getResources().getString(R.string.text_zeitWaehlen)
+                        && editText_ankunftZiel.getText().toString().equals(getResources().getString(R.string.text_zeitWaehlen))){
+                    Toast.makeText(FahrtBeenden.this,  "Bitte machen Sie die Zeitangaben, in dem Sie auf ZEIT AUSWÄHLEN drücken.", Toast.LENGTH_LONG).show();
+                } else if(editText_haltestelleZiel.getText().toString().isEmpty()){
+                    Toast.makeText(FahrtBeenden.this,  "Bitte geben Sie einen Haltestellennamen an.", Toast.LENGTH_LONG).show();
+                } else {
+                    boolean exportTrue = false;
 
-                boolean exportTrue = false;
+                    if (switch_fahrtExportieren.isChecked()) {
+                        exportTrue = true;
+                    }
 
-                if (switch_fahrtExportieren.isChecked()){
-                    exportTrue = true;
+                    collectedData.put("Ankunft Haltestelle", editText_endzeitFahrt.getText().toString());
+                    collectedData.put("Haltestellenname", editText_ankunftZiel.getText().toString());
+                    collectedData.put("Startzeit Fahrt", editText_haltestelleZiel.getText().toString());
+                    collectedData.put("KoordinatenEnde", lonDouble + ", " + latDouble);
+                    Intent intent = new Intent(FahrtBeenden.this, Umfrage.class);
+                    intent.putExtra("exportTrue", exportTrue);
+                    intent.putExtra("CollectedData", collectedData);
+                    startActivity(intent);
                 }
+            }
+        });
 
+        button_endzeitFahrt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenTimePicker.openTimePickerDialog(FahrtBeenden.this, editText_endzeitFahrt);
+            }
+        });
 
-                Intent intent = new Intent(FahrtBeenden.this, Umfrage.class);
-                intent.putExtra("exportTrue", exportTrue);
-                startActivity(intent);
+        button_ankunftZiel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenTimePicker.openTimePickerDialog(FahrtBeenden.this, editText_ankunftZiel);
             }
         });
     }
